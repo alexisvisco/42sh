09bf0ec46cde08a9ff8d9e06bf1796a9c30408de
alexisvisco <alexis.viscogliosi@outlook.fr>
2018-01-19 22:39:27 +0100

    add struct editor

--- includes/editor.h
+++ includes/editor.h
@@ -6,7 +6,7 @@
 /*   By: aviscogl <aviscogl@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/01/19 12:54:35 by aviscogl     #+#   ##    ##    #+#       */
-/*   Updated: 2018/01/19 12:55:34 by aviscogl    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/01/19 19:35:07 by aviscogl    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -14,5 +14,21 @@
 #ifndef EDITOR_H
 # define EDITOR_H
 
+typedef struct termios t_termios
+
+typedef struct	s_editor
+{
+	t_termios	origin;
+	char		*buf;
+	size_t		buf_len;
+	const char	*prompt;
+	size_t		plen;
+	size_t		pos;
+	size_t		len;
+    size_t		cols;
+    size_t		maxrows;
+    int			history_index;
+    t_mode		mode;
+}				t_editor
 
 #endif

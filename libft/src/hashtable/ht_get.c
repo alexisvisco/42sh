5ce4df578061133c397a0ea3aa6c8b107c1eea15
AlexisVisco <kwizzy@outlook.fr>
2018-01-29 13:40:01 +0100

    fix ht_get

--- libft/src/hashtable/ht_get.c
+++ libft/src/hashtable/ht_get.c
@@ -6,7 +6,7 @@
 /*   By: aviscogl <aviscogl@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/01/23 14:41:21 by aviscogl     #+#   ##    ##    #+#       */
-/*   Updated: 2018/01/23 15:17:44 by aviscogl    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/01/29 13:39:12 by aviscogl    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -18,5 +18,5 @@ void	*ht_get(t_hashtable *ht, const char *key)
 	t_node *node;
 
 	node = ht_has(ht, key);
-	return (node ? node->key : NULL);
+	return (node ? node->value : NULL);
 }

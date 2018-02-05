e6eba1bf03d2f7b280a34129347f0115ab284066
AlexisVisco <kwizzy@outlook.fr>
2018-02-05 11:46:00 +0100

    refactor handle_keys

--- src/editor/handle_keys.c
+++ src/editor/handle_keys.c
@@ -6,19 +6,30 @@
 /*   By: aviscogl <aviscogl@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/02/05 10:50:17 by aviscogl     #+#   ##    ##    #+#       */
-/*   Updated: 2018/02/05 10:50:17 by aviscogl    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/02/05 11:33:42 by aviscogl    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "editor.h"
 
+static int      add_history(const t_editor *l)
+{
+	if (l->options->has_history)
+	{
+		if (ft_strlen(l->buf) > 0)
+			history_add(l);
+		l->options->history_data->index = -1;
+	}
+	return ((int)l->len);
+}
+
 /*
 ** Handle the first key entered then redirect it to redirect_key_fn(..)
 ** If the key pressed is ENTER finish return the len of the buffer
 */
 
-int		handle_keys(t_editor *l)
+int          handle_keys(t_editor *l)
 {
 	char	c;
 	char	seq[10];
@@ -35,15 +46,11 @@ int		handle_keys(t_editor *l)
 				continue ;
 			}
 			ef_go_end(l);
-			if (l->options->has_history)
-			{
-				if (ft_strlen(l->buf) > 0)
-					history_add(l);
-				l->options->history_data->index = -1;
-			}
-			return ((int)l->len);
+			return add_history(l);
 		}
 		else
 			redirect_key_fn(l, c, seq);
 	}
 }
+
+
